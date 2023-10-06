7d90e2a80d6e0f09397f4a29b52a006daa25e184
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2023-10-06 10:07:33 +0200

    al mijn code

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,22 @@ modeSwitch.addEventListener("click" , () =>{
         
     }
 });
+
+// toggle button voor uitleengeschiednis 
+
+function toggleList(toggleIconId, listId) {
+
+    
+    const list = document.getElementById(listId);// parameters geven voor meer dan 1x gebruik
+    const icon = document.getElementById(toggleIconId);
+
+    if (list.style.display === "none" || list.style.display === "") { // checken of de list verbrogen of leeg is.
+        //als de list verborgen, kaat het zien (block)
+        list.style.display = "block";
+        icon.innerText = "-"; // kan op worden geklikt om het te verbergen 
+    } else {
+        //als de list te zien is verberg de list.
+        list.style.display = "none";
+        icon.innerText = "+"; // kan op worden geklik om het weer te laten zien 
+    }
+}
